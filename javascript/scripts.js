89e0e8a8cc78acf36d41a879110de9b0b9237339
DcodeNischal <dnischal11@gmail.com>
2022-11-19 10:34:26 +0545

    GodZer0

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20){
+$(document).ready(function () {
+  $("#bg-music").get(0).play();
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
       $(".goTop").fadeIn();
-    }
-    else{
+    } else {
       $(".navbar").removeClass("sticky");
       $(".goTop").fadeOut();
     }
   });
 
-  $(".goTop").click(function(){scroll(0,0)});
+  $(".goTop").click(function () {
+    scroll(0, 0);
+  });
 
-  $('.menu-toggler').click(function(){
+  $(".menu-toggler").click(function () {
     $(".navbar-menu").toggleClass("active");
   });
 
-   $('.navbar-menu').click(function(){
+  $(".navbar-menu").click(function () {
     $(this).toggleClass("active");
     $(".a").toggleClass("active");
   });
-
 });
 
-function change(flag1,flag2,flag3){
-  if(document.getElementById(flag1).style.display=="block"){
-    document.getElementById(flag1).style.display="none";
+function change(flag1, flag2, flag3) {
+  if (document.getElementById(flag1).style.display == "block") {
+    document.getElementById(flag1).style.display = "none";
+  } else {
+    document.getElementById(flag1).style.display = "block";
   }
-  else{
-    document.getElementById(flag1).style.display="block";
-  }
-  document.getElementById(flag2).style.display="none";
-  document.getElementById(flag3).style.display="none";
+  document.getElementById(flag2).style.display = "none";
+  document.getElementById(flag3).style.display = "none";
 }
