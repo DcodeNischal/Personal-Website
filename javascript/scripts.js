a83345249f8488cd1b7f3957e4bea2a022d5de29
Nischal Dhakal <dnischal11@gmail.com>
2023-02-21 19:47:33 +0545

    GodZer0

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -14,52 +14,13 @@ $(document).ready(function () {
   });
 
   $(".menu-toggler").click(function () {
+    $(this).toggleClass("active");
     $(".navbar-menu").toggleClass("active");
   });
 
   $(".navbar-menu").click(function () {
+    $(".menu-toggler").removeClass("active");
     $(this).toggleClass("active");
     $(".a").toggleClass("active");
   });
 });
-
-$(window).load(function () {
-  $(".preloader").delay(4000).fadeOut("slow");
-  $("#overlayer").delay(4000).fadeOut("slow");
-});
-
-function change(flag1, flag2, flag3) {
-  if (document.getElementById(flag1).style.display == "block") {
-    document.getElementById(flag1).style.display = "none";
-  } else {
-    document.getElementById(flag1).style.display = "block";
-  }
-  document.getElementById(flag2).style.display = "none";
-  document.getElementById(flag3).style.display = "none";
-}
-
-setInterval(function () {
-  x = new Date();
-  hour = x.getUTCHours() + 5;
-  minute = x.getUTCMinutes() + 45;
-  if (minute > 60) {
-    hour += 1;
-    minute -= 60;
-  }
-  ampm = "";
-  if (hour < 10) {
-    hour = "0" + hour;
-  }
-  if (minute < 10) {
-    minute = "0" + minute;
-  }
-  if (hour > 12) {
-    hour -= 12;
-    ampm = "PM";
-  } else {
-    ampm = "AM";
-  }
-  time = hour + ":" + minute + " " + ampm;
-
-  document.getElementById("clock").innerHTML = time;
-}, 1000);
